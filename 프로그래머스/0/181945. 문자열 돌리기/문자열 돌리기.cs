8e2f5487162a2038e96f84e3f76adcd7be064b0e
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-24 06:25:06 +0900

    [level 0] Title: 문자열 돌리기, Time: 15.95 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        
+        foreach(char c in input){
+            Console.WriteLine(c);
+        }
+    }
+}
