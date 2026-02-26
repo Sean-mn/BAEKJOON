eaf0b84cc7231796391205ff1fa755f39a11a04c
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-26 12:34:29 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int a, b, c;
+
+    while (true) {
+        cin >> a >> b >> c;
+
+        if (a == 0 && b == 0 && c == 0)
+            break;
+
+        if (a * a + b * b == c * c ||
+            a * a + c * c == b * b ||
+            b * b + c * c == a * a)
+            cout << "right\n";
+        else
+            cout << "wrong\n";
+    }
+
+    return 0;
+}
