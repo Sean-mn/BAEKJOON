6621fad6127395fa42ee4ab25936fa468d91be1e
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-26 21:59:50 +0900

    [Silver V] Title: 단어 정렬, Time: 508 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(string& a, string& b) {
+    if (a.size() == b.size())
+        return a < b;
+    return a.size() < b.size();
+}
+
+int main() {
+    cin.tie(NULL),ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<string> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end(), comp);
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    for (auto& s : v) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
